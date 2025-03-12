135becbc8fe3a54660985cad00446348dae6059e
fakerr <111339712+not-first@users.noreply.github.com>
2025-03-12 12:33:03 +1100

    adjust environment variable naming

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 import os
 
+
 class Config:
-    CACHE_INTERVAL = int(os.getenv("CACHE_INTERVAL", 3600))
-    ENABLE_AUTORESTIC_ICON = os.getenv("ENABLE_AUTORESTIC_ICON", "false").lower() == "true"
-    REPOS = os.getenv("REPOS", "").split(",")
+    CACHE_INTERVAL = int(os.getenv("RESTIC_CACHE_INTERVAL", 3600))
+    ENABLE_AUTORESTIC_ICON = os.getenv(
+        "ENABLE_AUTORESTIC_ICON", "false").lower() == "true"
+    REPOS = os.getenv("RESTIC_REPOS", "").split(",")
     RESTIC_CONFIG = {
         repo: {
             "path": os.getenv(f"{repo.upper().replace('-', '_')}_RESTIC_REPO"),
@@ -11,5 +13,6 @@ class Config:
         }
         for repo in REPOS
     }
+
 
 config = Config()
